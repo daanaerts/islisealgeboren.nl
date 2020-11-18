7058d8b9ba4479e0ffe9b881e42c14878d7442aa
Daan Aerts <daanaerts@gmail.com>
2020-11-18 13:48:58 +0100

    krassen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Canvas from "./Canvas";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="banner">
-          Nee
-        </h1>
-        <h5>lise is nog niet geboren</h5>
-      </header>
+      <div className="banner">
+        <h1>NEE</h1>
+        <h4>lise is nog niet geboren</h4>
+      </div>
+      <Canvas />
     </div>
   );
 }
---
+++ src/Canvas.tsx
@@ -0,0 +1,126 @@
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import "./canvas.css";
+
+interface CanvasProps {
+  width: number;
+  height: number;
+}
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
+const Canvas = ({ width, height }: CanvasProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isPainting, setIsPainting] = useState(false);
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(
+    undefined
+  );
+
+  const paint = useCallback(
+    (event: MouseEvent) => {
+      if (isPainting) {
+        const newMousePosition = getCoordinates(event);
+        if (mousePosition && newMousePosition) {
+          drawLine(mousePosition, newMousePosition);
+          setMousePosition(newMousePosition);
+        }
+      }
+    },
+    [isPainting, mousePosition]
+  );
+
+  // ...other stuff here
+
+  const drawLine = (
+    originalMousePosition: Coordinate,
+    newMousePosition: Coordinate
+  ) => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext("2d");
+    if (context) {
+      context.globalCompositeOperation = "destination-out";
+      context.strokeStyle = "rgba(255,255,255,1)";
+      context.lineWidth = 10;
+      context.lineJoin = "round";
+      context.beginPath();
+      context.moveTo(originalMousePosition.x, originalMousePosition.y);
+      context.lineTo(newMousePosition.x, newMousePosition.y);
+      context.closePath();
+
+      context.stroke();
+    }
+  };
+
+  const startPaint = useCallback((event: MouseEvent) => {
+    const coordinates = getCoordinates(event);
+    if (coordinates) {
+      setIsPainting(true);
+      setMousePosition(coordinates);
+    }
+  }, []);
+
+  const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas: HTMLCanvasElement = canvasRef.current;
+
+    const newCoordinates = {
+      x: event.pageX - canvas.offsetLeft,
+      y: event.pageY - canvas.offsetTop,
+    };
+    return newCoordinates;
+  };
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    canvas.addEventListener("mousedown", startPaint);
+    return () => {
+      canvas.removeEventListener("mousedown", startPaint);
+    };
+  }, [startPaint]);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    canvas.addEventListener("mousemove", paint);
+    return () => {
+      canvas.removeEventListener("mousemove", paint);
+    };
+  }, [paint]);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext("2d");
+    if (!context) {
+      return;
+    }
+    context.fillStyle = "grey";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
+
+  return (
+    <canvas ref={canvasRef} height={height} width={width} className="canvas" />
+  );
+};
+
+Canvas.defaultProps = {
+  width: "500px",
+  height: "400px",
+};
+
+export default Canvas;
